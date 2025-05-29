89b29aa5718cad0466a4c936193f83172201f53e
SchaleSensei-Repo <ashlovedawn@gmail.com>
2025-05-30 01:15:19 +0700

    moving libs.txt to proper place (root directory) and removing unnecessary file

--- libs/download-libs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Automatically download all JARs listed in libs.txt
-mkdir libs 2>nul
-for /f "tokens=*" %%A in (libs.txt) do (
-    echo Downloading %%A...
-    powershell -Command "Invoke-WebRequest -Uri %%A -OutFile libs\\%%~nxA"
-)
